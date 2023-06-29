bac21b27c6ea654ff2a8fe47681fcd6eba109bd0
XunDiYang <1920310178@qq.com>
2023-06-29 17:48:13 +0800

    Update Callback.java

--- app/src/main/java/com/socket/tcp_rawlongconn/model/Callback.java
+++ app/src/main/java/com/socket/tcp_rawlongconn/model/Callback.java
@@ -1,6 +1,6 @@
 package com.socket.tcp_rawlongconn.model;
 
 public interface Callback<T> {
-//    void onEvent( int code, String msg, T t);
+    //    void onEvent( int code, String msg, T t);
     void onEvent(CMessage cMessage, T t);
 }
