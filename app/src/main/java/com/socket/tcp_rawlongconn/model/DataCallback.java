6a551353edc49634432172222f767de982b981f6
XunDiYang <1920310178@qq.com>
2023-06-30 17:55:04 +0800

    Create DataCallback.java

---
+++ app/src/main/java/com/socket/tcp_rawlongconn/model/DataCallback.java
@@ -0,0 +1,12 @@
+package com.socket.tcp_rawlongconn.client.callback;
+
+import com.socket.tcp_rawlongconn.model.CMessage;
+
+import org.json.JSONException;
+/**
+ * 读数据回调
+ */
+public interface DataCallback {
+    void onData(CMessage data, int offset, int len) throws JSONException;
+}
+
