e4ef88cb0e89fc61e63cf8e046f86947e2a0093b
XunDiYang <1920310178@qq.com>
2023-07-03 17:48:18 +0800

    Update ServerActivity.java

--- app/src/main/java/com/socket/tcp_rawlongconn/server/view/ServerActivity.java
+++ app/src/main/java/com/socket/tcp_rawlongconn/server/view/ServerActivity.java
@@ -2,6 +2,7 @@ package com.socket.tcp_rawlongconn.server.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,13 +25,15 @@ public class ServerActivity extends AppCompatActivity {
         @Override
         public void onEvent(CMessage cMessage, Void unused) {
             if (cMessage.getCode() == 200) {
-                Toast.makeText(ServerActivity.this, "连接成功", Toast.LENGTH_SHORT).show();
                 if (cMessage.getType() == MsgType.TEXT && !cMessage.getMsg().isEmpty()) {
-                    txtRcvMsg.setText(txtRcvMsg.getText().toString() + "\n" + cMessage.getMsg());
-                }else if (cMessage.getType() == MsgType.PING){
-                    txtRcvMsg.setText(txtRcvMsg.getText().toString() + "\n" + "收到心跳包");
+                    Toast.makeText(ServerActivity.this, "连接成功", Toast.LENGTH_SHORT).show();
+                    String txt = "服务器已收到消息" + cMessage.getMsg() + "\n" + txtRcvMsg.getText().toString();
+                    txtRcvMsg.setText(txt);
                 }
-            } else {
+            }else if(cMessage.getCode() == 100){
+                Toast.makeText(ServerActivity.this, "客户端断开", Toast.LENGTH_SHORT).show();
+            }
+            else {
                 Toast.makeText(ServerActivity.this, "连接失败", Toast.LENGTH_SHORT).show();
             }
         }
@@ -43,11 +46,15 @@ public class ServerActivity extends AppCompatActivity {
         Intent intent = getIntent();
         serverIp = intent.getStringExtra("serverIp");
         serverPort = intent.getIntExtra("serverPort", 8888);
+
         txtlocalip = findViewById(R.id.localip);
-        txtRcvMsg = findViewById(R.id.rcvMsg);
         txtlocalip.setText(toString());
+        txtRcvMsg = findViewById(R.id.rcvMsg);
+        txtRcvMsg.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+
         mEchoServer = new EchoServer(serverPort, rcvMsgCallback);
-        mEchoServer.run();
+        mEchoServer.start();
     }
 
     @NonNull
