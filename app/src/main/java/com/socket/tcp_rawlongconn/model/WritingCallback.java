2585fadc9500ce844765e4d57e1625a07c0c58a1
XunDiYang <1920310178@qq.com>
2023-07-03 17:38:36 +0800

    Update WritingCallback.java

--- app/src/main/java/com/socket/tcp_rawlongconn/model/WritingCallback.java
+++ app/src/main/java/com/socket/tcp_rawlongconn/model/WritingCallback.java
@@ -1,9 +1,12 @@
 package com.socket.tcp_rawlongconn.client.callback;
+
+import com.socket.tcp_rawlongconn.model.CMessage;
+
 /**
  * 写数据回调
  */
 public interface WritingCallback {
     void onSuccess();
 
-    void onFail(byte[] data, int offset, int len);
+    void onFail(CMessage cMessage);
 }
