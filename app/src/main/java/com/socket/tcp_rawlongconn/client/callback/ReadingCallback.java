fad94b27ea2c627777f001ea9b8f60fe2f0384e5
XunDiYang <1920310178@qq.com>
2023-07-05 15:20:12 +0800

    Update and rename DataCallback.java to ReadingCallback.java

--- app/src/main/java/com/socket/tcp_rawlongconn/client/callback/ReadingCallback.java
+++ app/src/main/java/com/socket/tcp_rawlongconn/client/callback/ReadingCallback.java
@@ -2,11 +2,10 @@ package com.socket.tcp_rawlongconn.client.callback;
 
 import com.socket.tcp_rawlongconn.model.CMessage;
 
-import org.json.JSONException;
 /**
  * 读数据回调
  */
-public interface DataCallback {
+public interface ReadingCallback {
     void onData(CMessage data);
 }
 
