af106d2566fc89f26cea33c487d9938bc4e62073
XunDiYang <1920310178@qq.com>
2023-07-05 15:24:07 +0800

    Update EchoServer.java

--- app/src/main/java/com/socket/tcp_rawlongconn/server/service/EchoServer.java
+++ app/src/main/java/com/socket/tcp_rawlongconn/server/service/EchoServer.java
@@ -1,5 +1,6 @@
 package com.socket.tcp_rawlongconn.server.service;
 
+
 import android.os.Handler;
 import android.util.Log;
 
@@ -13,74 +14,81 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer {
-    private static final String TAG = "EchoServer";
+        private final String TAG = "EchoServer";
 
+    private final String serverIp;
     private final int mPort;
     private Callback<Void> rcvMsgCallback;
-    private volatile boolean running = false;
-    private Thread connWatchDog;
+
     private Handler handler;
+    /********************************************************/
+    /*
+     *   以下使用Executors实现(线程池)（readObject）
+     *
+     *
+     * */
+    private final ExecutorService mExecutorService;
+    private ServerSocket serverSocket;
 
-//    3s的判断
-    private long receiveTimeDelay=3000;
+    private volatile boolean running = true;
 
-    public EchoServer(int port, Callback<Void> rcvMsgCallback) {
+    public EchoServer(String serverIp, int port, Callback<Void> rcvMsgCallback) {
+        this.serverIp = serverIp;
         mPort = port;
         this.rcvMsgCallback = rcvMsgCallback;
         handler = new Handler();
-    }
-
-    @SuppressWarnings("deprecation")
-    public void stopServer() {
-        if (running){
-            running = false;
-        }
-        if (connWatchDog != null){
-            connWatchDog.stop();
+        mExecutorService = Executors.newFixedThreadPool(5);
+        try {
+            serverSocket = new ServerSocket(mPort);
+            Log.e(TAG, "服务器启动成功");
+        } catch (IOException e) {
+            Log.e(TAG, "服务器启动失败" + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 
     public void start() {
-        if (running) return;
-        running = true;
-        connWatchDog = new Thread(new ConnWatchDog());
-        connWatchDog.start();
+        mExecutorService.execute(new ConnWatchDog());
+    }
+
+    public void stop() throws IOException {
+        running = false;
+        mExecutorService.shutdown();
+        serverSocket.close();
+        CMessage cMessage = new CMessage();
+        cMessage.setCode(500);
+        handler.post(() -> rcvMsgCallback.onEvent(cMessage, null));
     }
 
     class ConnWatchDog implements Runnable {
-
         @Override
         public void run() {
-            ServerSocket serverSocket;
-            try {
-                serverSocket = new ServerSocket(mPort, 5);
-                Log.d(TAG, "服务器启动成功");
-                while (running) {
-                    try {
-                        Socket client = serverSocket.accept();
-                        Log.d(TAG, "有客户端请求链接");
-                        new Thread(new HandleClient(client)).start();
-                    } catch (IOException e) {
-                        CMessage cMessage = new CMessage();
-                        cMessage.setCode(300);
-                        handler.post(() -> rcvMsgCallback.onEvent(cMessage, null));
-                        Log.e(TAG, "有客户端链接失败" + e.getMessage());
-                    }
+            while (running) {
+                try {
+                    Socket client = serverSocket.accept();
+                    client.setKeepAlive(true);
+                    Log.d(TAG, "有客户端请求链接");
+                    CMessage cMessage = new CMessage();
+                    cMessage.setCode(100);
+                    handler.post(() -> rcvMsgCallback.onEvent(cMessage, null));
+                    mExecutorService.submit(new HandleClient(client));
+                } catch (IOException e) {
+                    CMessage cMessage = new CMessage();
+                    cMessage.setCode(400);
+                    handler.post(() -> rcvMsgCallback.onEvent(cMessage, null));
+                    Log.e(TAG, "有客户端链接失败" + e.getMessage());
                 }
-            } catch (IOException e) {
-                Log.e(TAG, "服务器启动失败" + e.getMessage());
-                stopServer();
             }
-
         }
     }
 
-    class HandleClient implements Runnable {
+    private class HandleClient implements Runnable {
         Socket s;
-        boolean run = true;
-
+        volatile boolean run = true;
         long lastReceiveTime = System.currentTimeMillis();
 
         public HandleClient(Socket s) {
@@ -89,46 +97,40 @@ public class EchoServer {
 
         @Override
         public void run() {
-            while (running && run) {
-//                TODO: 心跳包超时
-                if(System.currentTimeMillis()-lastReceiveTime>receiveTimeDelay){
-                    closeClientSocket();
-                }else{
-                    try {
-                        InputStream in = s.getInputStream();
-                        if (in.available() > 0) {
-                            ObjectInputStream ois = new ObjectInputStream(in);
-                            Object obj = ois.readObject();
-                            Log.d(TAG, "收到消息");
-                            lastReceiveTime = System.currentTimeMillis();
-                            CMessage cMessage = (CMessage) obj;
-                            Object out = new CMessage(cMessage.getTo(),cMessage.getFrom(),cMessage.getCode(), cMessage.getType(),cMessage.getMsg());
-                            cMessage.setMsg("来自"+ cMessage.getFrom()+"客户端: "+cMessage.getMsg());
-                            if(cMessage.getCode()!=200 || cMessage.getType() == MsgType.TEXT){
-                                handler.post(() -> rcvMsgCallback.onEvent(cMessage, null));
-                            }
-                            else if(cMessage.getType() == MsgType.PING){
-                                Log.d(TAG,"收到心跳包");
-                            }
-                            if (out != null) {
-                                ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
-                                oos.writeObject(out);
-                                oos.flush();
-                                Log.d(TAG, "发送消息");
-                            }
-                        } else {
-                            Thread.sleep(10);
+            try {
+                while (running && run) {
+                    InputStream in = s.getInputStream();
+                    if (in.available() > 0) {
+                        ObjectInputStream ois = new ObjectInputStream(in);
+                        Object obj = ois.readObject();
+                        Log.d(TAG, "收到消息");
+                        lastReceiveTime = System.currentTimeMillis();
+                        CMessage cMessage = (CMessage) obj;
+                        Object out = new CMessage(cMessage.getTo(), cMessage.getFrom(), cMessage.getCode(), cMessage.getType(), cMessage.getMsg());
+                        cMessage.setMsg("来自" + cMessage.getFrom() + "客户端: " + cMessage.getMsg());
+                        if (cMessage.getCode() != 200 || cMessage.getType() == MsgType.TEXT) {
+                            handler.post(() -> rcvMsgCallback.onEvent(cMessage, null));
                         }
-                    } catch (Exception e) {
-                        CMessage cMessage = new CMessage();
-                        cMessage.setCode(300);
-                        handler.post(() -> rcvMsgCallback.onEvent(cMessage, null));
-                        Log.e(TAG, "handleClient: ", e);
-                        closeClientSocket();
+                        if (out != null) {
+                            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+                            oos.writeObject(out);
+                            oos.flush();
+                            Log.d(TAG, "发送消息");
+                        }
+                    } else {
+                        Thread.sleep(10);
                     }
                 }
-
+            } catch (Exception e) {
+                CMessage cMessage = new CMessage();
+                cMessage.setCode(400);
+                handler.post(() -> rcvMsgCallback.onEvent(cMessage, null));
+                Log.e(TAG, "handleClient: ", e);
+                closeClientSocket();
+            } finally {
+                closeClientSocket();
             }
+            Log.d(TAG, "结束处理数据");
         }
 
         private void closeClientSocket() {
@@ -142,11 +144,10 @@ public class EchoServer {
                     e.printStackTrace();
                 }
             }
-            handler.post(() -> rcvMsgCallback.onEvent(new CMessage("","",100,MsgType.CONNECT,""), null));
+//            handler.post(() -> rcvMsgCallback.onEvent(new CMessage("", "", 400, MsgType.CONNECT, ""), null));
             Log.e(TAG, "关闭：" + s.getRemoteSocketAddress());
         }
     }
-
 }
 
 
